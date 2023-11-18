731e2e380c263260a44d86e1026c6310a9235990
Joey V <joey03v@gmail.com>
2023-11-17 21:12:22 -0600

    finished index file code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,108 @@
 // TODO: Include packages needed for this application
+const inquirer = require ('inquirer');
+const { writeFile } = require('fs').promises;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the title of your project?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Describe your project."
+    },
+    {
+        type: 'input',
+        name: 'instalation',
+        message: "What steps do you take to install this project?"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "How do you use this project?"
+    },
+    {
+        type: 'input',
+        name: 'futureImplementations',
+        message: "What feaures do you plan on implementing in the future?"
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: "which license did you use?"
+    },
+    {
+        type: 'input',
+        name: 'githubUser',
+        message: "Enter your github username: "
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email?"
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: "Its imortant to add a RELATIVE path to a live screenshot of your application: "
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: "Please include the appication url: "
+    }
+]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateReadMe = ({title,description,instalation,usage,futureImplementations,license,githubUser,email}) =>
+`# ${title}
+
+## licence
+${license}
+
+## Description
+${description}
+
+# Table of Contents
+*Instalation
+*Usage
+*Future Implementations
+*Contact For Questions
+*Live Screenshot
+*Link to Application
+
+
+## Instalation
+${instalation}
+
+## Usage
+${usage}
+
+## future Implementations
+${futureImplementations}
+
+## Contact For Questions
+${githubUser}
+${email}
+
+## Live Screenshot
+![Alt text](<${screenshot}>)
+
+## Link to Application
+[a link] (${link}) `;
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+    .then((answers) => writeFile('README.md', generateReadMe(answers)))
+    .then(() => console.log('Successfully created your ReadMe!'))
+    .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
